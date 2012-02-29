1f7d14f0a1c8ea027565e31bb90957f6c31455d1
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-29 19:14:51 +0000

    cloud update

--- cloud/main.js
+++ cloud/main.js
@@ -2,31 +2,30 @@
  * main entry of cloud side
  */
 
-
 /**
  * Create a new Request ticket in Zendesk with requester
  */
-function newRequest(param){
-	return zendesk.newRequest(param.subject,param.des,param.email);
+function newRequest(param) {
+	return zendesk.newRequest(param.subject, param.des, param.email);
 }
 
 /**
  * Retrieve all request tickets
  */
-function listUserRequests(param){
+function listUserRequests(param) {
 	return zendesk.listUserRequests(param.email);
 }
 
 /**
  * Calculate mortgage with user input
  */
-function getMortgage(param){
-	return mortgage.getMortgage(param.years,param.interest,param.loanAmount,param.tax,param.insurance);
+function getMortgage(param) {
+	return mortgage.getMortgage(param.years, param.interest, param.loanAmount, param.tax, param.insurance);
 }
 
 /**
  * Get stock symbol and detailed information by company name
  */
-function getStockInfo(param){
+function getStockInfo(param) {
 	return stock.getStockInfo(param.name);
 }
